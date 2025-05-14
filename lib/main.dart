916462ce4688d24d5f83a90b747c6e31edf8666c
Nicollas1705 <nicollasmota1705@gmail.com>
2025-05-14 17:40:21 -0300

    Added comment about permissions to run in debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'presentation/controllers/app_controller.dart';
 import 'presentation/main_app.dart';
 
+// * Note: if the app doesn't run correctly in debug mode on Android, check out the file: android/app/src/main/AndroidManifest.xml
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MainApp(appController: AppController()));
